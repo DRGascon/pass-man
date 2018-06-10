c34a89e72be61100a13dd9ff9304abfb8634ff86
Dave Gascon <davergascon@gmail.com>
2018-06-10 08:56:26 -0400

    Adding secret locking and unlocking to users
    Additional unit tests

--- unit-tests/tc-user.rb
+++ unit-tests/tc-user.rb
@@ -9,20 +9,28 @@ require 'minitest/autorun'
 
 class TC_UserTest < MiniTest::Test
 
-    def setup
-        digest = OpenSSL::Digest::SHA512.new
-        password = "password1234"
-        @password_key = OpenSSL::PKCS5.pbkdf2_hmac(password, "1234", 20000, 32, digest)
+    def test_locked_init
+        new_user = User.new "dgascon", 1234, "this is some special secret"
+        assert new_user.unlocked == false
     end
 
-    def test_locked_init
-        new_user = User.new "dgascon", 1234, @password_key
+    def test_locking_user
+        new_user = User.new "dgascon", 1234, "this is some special secret"
+        new_user.lock("password1234")
         assert new_user.unlocked == false
     end
 
     def test_unlocking_user
-        new_user = User.new "dgascon", 1234, @password_key
+        new_user = User.new "dgascon", 1234, "this some special secret"
+        new_user.lock("password1234")
         new_user.unlock("password1234")
         assert new_user.unlocked == true
+    end
+
+    def test_back_unlock
+        new_user = User.new "dgascon", 1234, "this some special secret"
+        new_user.lock("password1234")
+        new_user.unlock("password1235")
+        assert new_user.unlocked == false
     end
 end
--- user.rb
+++ user.rb
@@ -10,8 +10,9 @@ require 'openssl'
 # Secrets remain encrypted until the user is authenticated
 ################################################################################
 class User
-    attr_accessor :name, :id
+    attr_accessor :name, :id, :iv, :auth_tag
     attr_reader :secret, :unlocked
+
     ############################################################################
     # Initialize a new user
     #
@@ -32,18 +33,82 @@ class User
     ############################################################################
     def unlock(password)
         Logging.logger.info "Trying to unlock user " + @name
+        secret_key = generate_key(password)
+        # Now try to decrypt the secret
+        decrypted_secret = decrypt_secret(secret_key, @iv, @auth_tag)
+        # Did we successfully get the secret?
+        if !decrypted_secret.nil?
+            @secret = decrypted_secret
+        end
+        @unlocked = !decrypted_secret.nil?
+    end
+
+    ############################################################################
+    # Generate a key based off a password, using parts of the user for a salt
+    ############################################################################
+    def generate_key(password)
         digest = OpenSSL::Digest::SHA512.new
-        # First generate the key based on the password
+        # Generate the key based on the password
         secret_key = OpenSSL::PKCS5.pbkdf2_hmac(password, @id.to_s, 20000, 32, digest)
-        # Is this our expected secret?
-        @unlocked = Utils.equal_time_compare(secret_key, @secret)
-        Logging.logger.info "Unlock attempt for " + @name + " result " + @unlocked.to_s
+    end
+
+    ############################################################################
+    # Decrypt the user's secret key
+    ############################################################################
+    def decrypt_secret(key, iv, auth_tag)
+        # Get the cipher
+        cipher = OpenSSL::Cipher::AES.new(256, :GCM)
+        # Set to decrypt
+        cipher.decrypt
+        # Setup the cipher parameters
+        cipher.iv = iv
+        cipher.key = key
+        cipher.auth_tag = auth_tag
+        cipher.auth_data = @name + @id.to_s
+        # Try to do the decryption, if the tags fail to verify we'll get a
+        # CipherError thrown
+        begin
+            decrypted_secret = cipher.update(@secret) + cipher.final
+        rescue OpenSSL::Cipher::CipherError
+            Logging.logger.error "Failed to decrypt secret for user " + @name + " tag " + @auth_tag.unpack('H*').first
+            decrypted_secret = nil
+        end
     end
 
     ############################################################################
     # Lock the user's secret with a password
     ############################################################################
     def lock(password)
+        cipher = OpenSSL::Cipher::AES.new(256, :GCM)
+        # If there's no IV, generate one
+        @iv = @iv.nil? ? cipher.random_iv : @iv
+        key = generate_key(password)
+        @auth_tag = encrypt_secret(key, @iv)
         @unlocked = false
     end
+
+    ############################################################################
+    # Encrypt the user's secret key
+    ############################################################################
+    def encrypt_secret(key, iv)
+        # Get the cipher
+        cipher = OpenSSL::Cipher::AES.new(256, :GCM)
+        # Set to decrypt
+        cipher.encrypt
+        # Setup the cipher parameters
+        cipher.iv = iv
+        cipher.key = key
+        cipher.auth_data = @name + @id.to_s
+        # Try to do the decryption, if the tags fail to verify we'll get a
+        # CipherError thrown
+        begin
+            @secret = cipher.update(@secret) + cipher.final
+        rescue OpenSSL::Cipher::CipherError
+            Logging.logger.error "Failed to encrypt secret for user " + @name
+        end
+        Logging.logger.error "Encrypted secret for user " + @name + " tag " + cipher.auth_tag.unpack('H*').first
+        cipher.auth_tag
+    end
+
+    private :decrypt_secret, :encrypt_secret
 end
